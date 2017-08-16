7a6cd9e125013905b1976bcae53cd15beab15ad1
Florian Paul Schmidt <mista.tapas@gmx.net>
2017-08-16 11:01:34 +0200

    rename

--- nn3/nn_initialize_weights_gaussian.m
+++
@@ -1,8 +0,0 @@
- 
-function new_nn = nn_initialize_weights_gaussian(nn)
-    new_nn = nn;
-    
-    for layer = 1:rows(new_nn)
-        new_nn{layer}.forward_weights = randn(size(new_nn{layer}.forward_weights));
-    end
-end
